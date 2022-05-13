e427707c274d680c4ae41d0eb3c86d4a6b9a610c
kenza-sif <74767829+kenza-sif@users.noreply.github.com>
2022-05-13 17:06:33 +0200

    login

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, request 
+from jwt_utils import *
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +7,21 @@ def hello_world():
 	x = 'world'
 	return f"Hello, {x}"
 
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+   
+@app.route('/login',  methods=['POST'])
+def Login(): 
+    payload = request.get_json()
+    token= build_token() 
+    if payload["password"]== "Vive l'ESIEE !":
+        return {'token' : token}, 200
+    else :
+        return '', 401
+
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
-    
+
+
+
